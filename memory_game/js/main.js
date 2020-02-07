b7648b58f965dbe254e4a489557c5f01a193c1f4
Brittany Price <brittanyprice@Brittanys-MacBook-Air.local>
2020-02-06 17:07:50 -0800

    created game logic

---
+++ memory_game/js/main.js
@@ -0,0 +1,15 @@
+let cards = ["queen", "queen", "king", "king"];
+let cardsInPlay = [];
+let cardOne = cards[0];
+cardsInPlay.push('cardOne');
+console.log("User flipped " + cardOne);
+let cardTwo = cards[2];
+cardsInPlay.push('cardTwo');
+console.log("User flipped " + cardTwo);
+
+if (cardsInPlay.length === 2) {
+} if (cardsInPlay[0] === cardsInPlay[2]) {
+alert("You found a match!");
+} else {
+	alert("Sorry, try again.");
+}
